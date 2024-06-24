6b1edc40b1baee2ed8b4c2373bbb72955114d7aa
supraja2030 <bsupraja7891@gmail.com>
2024-06-24 13:10:13 +0530

    print even numbers

---
+++ src/loops/printEven.java
@@ -0,0 +1,13 @@
+package loops;
+
+public class printEven {
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=10;i++){
+            if(i%2==0){
+                System.out.println("Even number : " + i);
+            }
+
+        }
+    }
+}

0b21520b8affb7f654045a4d55fa6569c7993be9
supraja2030 <bsupraja7891@gmail.com>
2024-06-24 23:18:19 +0530

    int data type

---
+++ src/dataTypes/integer.java
@@ -0,0 +1,14 @@
+package dataTypes;
+
+public class integer {
+    public int number(){
+        int a = 10;
+        return a;
+    }
+    public static void main(String[] args) {
+        integer obj = new integer();
+        int k = obj.number();
+        System.out.println(k);
+
+    }
+}

a0c6c97b74220db8047fdf838487fbd90ffcc287
supraja2030 <bsupraja7891@gmail.com>
2024-06-24 13:16:52 +0530

    multiplying 2 with a given numbers

---
+++ src/loops/multiplyingNumber.java
@@ -0,0 +1,10 @@
+package loops;
+
+public class multiplyingNumber {
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=10;i++){
+            System.out.println(i*2);
+        }
+    }
+}

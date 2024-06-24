8ccaff7b9bbcff88034ba6ac0d4a966c9bf51150
supraja2030 <bsupraja7891@gmail.com>
2024-06-24 13:05:25 +0530

    sum of numbers

---
+++ src/loops/sumOfNumbers.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class sumOfNumbers {
+    public static void main(String[] args) {
+        int i,sum=0;
+        for(i=1;i<=10;i++){
+            sum=sum+i;
+            System.out.println(sum);
+        }
+
+    }
+}

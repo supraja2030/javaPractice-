a8b7c8f8190c2e49f42c61c3c3ba16e87d1c39e9
supraja2030 <bsupraja7891@gmail.com>
2024-06-25 19:02:26 +0530

    using final in variable

---
+++ src/Final/finalVariable.java
@@ -0,0 +1,10 @@
+package Final;
+
+public class finalVariable {
+    public static void main(String[] args) {
+         final int number = 10;
+         //number = 20; throws compile time error
+        System.out.println(number);
+
+    }
+}

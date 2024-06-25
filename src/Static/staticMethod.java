220c584e907bf0a77868bcd457cb627d251a97e5
supraja2030 <bsupraja7891@gmail.com>
2024-06-25 17:54:05 +0530

    static method

---
+++ src/Static/staticMethod.java
@@ -0,0 +1,11 @@
+package Static;
+
+public class staticMethod {
+    public static int display(){
+        int a = 10;
+        return a;
+    }
+    public static void main(String[] args) {
+        System.out.println(display());
+    }
+}
